2eeb4ea97c9e3e4940a52dd883f7b0e4aa0375fe
Mattes382 <MatejPospisill@email.cz>
2023-06-26 03:32:12 +0200

    repository

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use App\Entity\Money;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
+/**
+ * @extends ServiceEntityRepository<User>
+ */
 class UserRepository extends ServiceEntityRepository implements UserRepositoryInterface
 {
     public function __construct(ManagerRegistry $registry)
